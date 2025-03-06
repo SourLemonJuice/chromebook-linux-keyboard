473a22062e8c05a9d7dbf8dff48a6beecc0d7357
酸柠檬猹/SourLemonJuice <SourLemonJuice233@outlook.com>
2025-03-06 09:51:21 +0800

    更新 make.sh 脚本以让用户选择要使用的功能键布局

--- make.sh
+++ make.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
-cat keyd/00-*.conf \
-    keyd/10-function-a.conf \
-    keyd/20-*.conf \
-    > chromebook.conf
+cd keyd/
+
+func_conf="10-function-a.conf"
+echo "Choose the function key layout file:"
+
+select conf in 10-function-*.conf; do
+    echo "Selected: ${conf}"
+    func_conf="$conf"
+    break
+done
+
+cat 00-*.conf \
+    "${func_conf}" \
+    20-*.conf \
+    > ../chromebook.conf
